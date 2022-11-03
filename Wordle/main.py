d308ea57ddbba32935bcadd8f4c65174f1314c9a
Tejkin <Thomas1467@hotmail.com>
2022-11-03 15:31:22 +0800

    added docstring to functions

--- Wordle/main.py
+++ Wordle/main.py
@@ -10,6 +10,9 @@ TARGET_WORDS_PATH = "C:/Users/CHENTH/Documents/Python/Wordle/Wordle/word-bank/ta
 
 # Play the game
 def play():
+    '''
+    Combines functions and starts the game
+    '''
     attempts = 0
     target_word = get_target_word()
     if cheats == True:
@@ -35,6 +38,12 @@ def play():
 
 # Choosing Random Target Word
 def get_target_word():
+    '''
+    Opens target_words.txt and begins choosing a random word to be the target word of the game
+
+    Returns:
+        str: target_word
+    '''
     fh = open(TARGET_WORDS_PATH, "r")
     words_list = []
     for words in fh:
@@ -45,10 +54,26 @@ def get_target_word():
 
 # Ask for guess
 def guess_input():
+    '''
+    Ask for user to input guess
+
+    Returns:
+        str: The user input/guess
+    '''
     return (input("Enter guess: "))
 
 # Check for a valid word
 def check_valid_word(guess_word):
+    '''
+    Checks user input and compares it to all_words.txt, checking if user input is a valid 5 letter word in the english dictionary
+
+    Args:
+        guess_word (str): takes in user's guess word/input
+
+    Returns:
+        bool: Returns true for valid word and false for invalid word
+
+    '''
     fh = open(ALL_WORDS_PATH, "r")
     valid_words = []
     for words in fh:
@@ -62,6 +87,15 @@ def check_valid_word(guess_word):
 
 # Check for correct guess
 def is_correct(guess_word, target_word, attempts):
+    '''
+    Skips scoring process and checks if guess word directly matches the target word
+
+    Args:
+        guess_word (str): User input/guess
+        target_word (str): Target word for the game
+        attempts (int): Attempts left in the game
+
+    '''
     if guess_word == target_word:
         if attempts == 1:
             print("Correct word is: " + Colours.GREEN + target_word + Colours.END + "\nCongratul---... Wait! You got it in one attempt??")
@@ -77,6 +111,19 @@ def is_correct(guess_word, target_word, attempts):
 
 # Scores guess
 def score_guess(guess_word, target_word):
+    '''
+    Scores the user's guess with 
+    2: correct letter and position
+    1: correct letter, wrong position
+    0: wrong letter and position
+
+    Args:
+        guess_word (str): user input/guess
+        target_word (str): The target word of the game
+
+    Returns:
+        list: list format with numbered score in each position, default score [0, 0, 0, 0, 0]
+    '''
     position = [0, 0, 0, 0, 0]
     guess_letter = list(guess_word)
     target_letter = list(target_word)
@@ -91,6 +138,15 @@ def score_guess(guess_word, target_word):
 
 # Change scores to be readable
 def read_score(score, target_letter, guess_letter, display):
+    '''
+    Reads the score returned by score_guess() and processes it to become readable to the user
+
+    Args:
+        score (list:'int'): Scoring in list form returned by score_guess()
+        target_letter (list:'str'): Letter corresponding to index in the target word
+        guess_letter (list: 'str'): Letter corresponding to index in the guess word
+        display (list: 'str'): Changes display to display correctly guessed letters
+    '''
     for i in score:
         if i == 2:
             colour = Colours.GREEN
@@ -117,6 +173,9 @@ def read_score(score, target_letter, guess_letter, display):
 
 # Class holding colors
 class Colours:
+    '''
+    Class containing strings to modify colour in terminal
+    '''
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     RED = '\033[91m'
@@ -126,12 +185,18 @@ class Colours:
 
 # Print keyboard
 def print_keyboard():
+    '''
+    Prints modified keyboard to look aesthetically spaced in terminal
+    '''
     print("   " + "  ".join(keys1))
     print("     " + "  ".join(keys2))
     print("\t" + "  ".join(keys3))
 
 # Colour keyboard
 def keyboard(colour, letter):
+    '''
+    Modifies keyboard display to show correctly guessed letters with corresponding colours
+    '''
     letter = letter.upper()
     while True:
         if letter in keys1:
@@ -157,6 +222,12 @@ def keyboard(colour, letter):
 
 # Print out attempts left
 def attempts_left(attempts):
+    '''
+    Calculates the attempts left by the user
+
+    Args:
+        attempts (int): The attempts used by the user
+    '''
     attempts_left_over = MAX_ATTEMPTS - attempts
     if attempts_left_over >= 3:
         print("You have " + Colours.GREEN + str(attempts_left_over) + Colours.END + " attempts left")
@@ -167,6 +238,9 @@ def attempts_left(attempts):
 
 # Start asking for command input
 def start():  
+    '''
+    Starts the start-up interface for the game and sets the global variables to be used in game
+    '''
     global MAX_ATTEMPTS
     MAX_ATTEMPTS = 6
     global cheats
@@ -206,6 +280,9 @@ def start():
 
 # User input to play again after finished game
 def play_again():
+    '''
+    Prompts user input to choose to play again after the game finishes
+    '''
     user_input = input("Would you like to start again?: Y/N \n")
     if user_input.upper() == "Y":
         start()
@@ -213,6 +290,12 @@ def play_again():
         exit()
 
 def list_guess(score):
+    '''
+    Display all guesses by the user in the current game with appropriate colouring
+
+    Args:
+        score (list'str'): Score for the users guess/input
+    '''
     guess_list.append(score)
     print("\n\t".join(guess_list) + "\n")
 
